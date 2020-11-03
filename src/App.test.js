9cd186c1eceaf9822de476cb7fd115beb45f9b4b
fatimaavilesfa <fatimaaviles1612@gmial.com>
2020-11-02 20:04:58 -0700

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Learning docker with my husband!!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Learning docker with my husband!!/i);
   expect(linkElement).toBeInTheDocument();
 });
